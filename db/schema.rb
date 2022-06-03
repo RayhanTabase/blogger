400f040dbc146a8c5bedc2d912c37adb1b9388bb
salimabdulai <maouslayer@gmail.com>
2022-06-03 20:38:30 +0000

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_03_173054) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_03_192202) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -39,6 +39,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_03_173054) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "author_id", null: false
+    t.string "title"
+    t.text "text"
+    t.integer "comments_counter"
+    t.integer "likes_counter"
     t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
