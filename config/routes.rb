e6ecf18efa16e1ac8a22034d9757605beb395b3e
salimabdulai <maouslayer@gmail.com>
2022-06-11 00:28:55 +0000

    Add forms amd methods to display and save posts, comments and likes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
+
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
   end
+
+  resources :posts do
+    resources :comments, only: [:create]
+    resources :likes, only: [:create]
+  end
+
+  resources :posts, only: [:new, :create]
+
+
   root 'users#index'
 end
